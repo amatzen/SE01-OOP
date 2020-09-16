948911c650e6f1fc05845fbbef838c554a542b9b
Alexander Matzen <alexander@amatzen.dk>
2020-09-16 09:59:29 +0200

    Areal af Cirkler

---
+++ Week38/src/arrays/ArealAfCirkler.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class ArealAfCirkler {
+    public static void main(String[] args) {
+        // Taget fra min udarbejdelse i uge 37
+        // https://github.com/amatzen/SE01-OOP/blob/master/Week37/src/dk/alexander/ovelser/week37/Main.java
+
+        double[] data = {1, 3, 5};
+        double pi = 3.14;
+
+        for (double i: data) {
+            System.out.println("3.14 * "+i+"^2 = " + Math.pow(i, 2) * pi);
+        }
+    }
+}
